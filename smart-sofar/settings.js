864c60eaf17ac62323968db5a94e3e97b185ddac
tapion69 <151565403+tapion69@users.noreply.github.com>
2026-03-29 12:18:22 +0200

    Create settings.js

---
+++ smart-sofar/settings.js
@@ -0,0 +1,45 @@
+module.exports = {
+  uiPort: 1892,
+  userDir: "/data",
+  flowFile: "/data/flows.json",
+
+  credentialSecret: false,
+
+  adminAuth: {
+    type: "credentials",
+    users: [{
+      username: "pi",
+      password: "$2a$12$fTPLydFlFsX7N6x8zqbnke7eIGdXDHWp4uzzqSdpOJClmFDEw1Ifu",
+      permissions: "*"
+    }]
+  },
+
+  nodesDir: ["/opt/node_modules"],
+
+  functionGlobalContext: {
+    crypto: require("crypto"),
+    fs: require("fs"),
+    path: require("path")
+  },
+
+  contextStorage: {
+    default: {
+      module: "memory"
+    },
+    persistent: {
+      module: "localfilesystem"
+    }
+  },
+
+  editorTheme: {
+    projects: { enabled: false }
+  },
+
+  logging: {
+    console: {
+      level: "warn",
+      metrics: false,
+      audit: false
+    }
+  }
+};
